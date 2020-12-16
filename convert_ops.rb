cdccdf83efaf2bd2bf34a2095051338b4faefeee
HirohitoHigashi <higashi@s-itoc.jp>
2020-12-16 15:25:47 +0900

    fix prog name.

--- convert_ops.rb
+++ convert_ops.rb
@@ -3,7 +3,7 @@
 # convert mruby's ops.h to mruby/c's enum
 #
 # (usage)
-#   make_opcode.rb PATH/TO/MRUBY/include/mruby/ops.h > tempfile.c
+#   convert_ops.rb PATH/TO/MRUBY/include/mruby/ops.h > tempfile.c
 #
 #  and merge manually opcode.h and tempfile.c
 #
@@ -12,7 +12,7 @@
 # main
 #
 if ARGV.size != 1
-  puts "usage: make_opcode.rb PATH/TO/MRUBY/include/mruby/ops.h > tempfile.c"
+  puts "usage: convert_ops.rb PATH/TO/MRUBY/include/mruby/ops.h > tempfile.c"
   exit
 end
 
@@ -23,6 +23,8 @@ File.open( ARGV[0] ) {|file|
     if /OPCODE\((\w+)\s*,\s*(\w+)\s*\)\s*\/\*(.+)\*\// =~ txt
       opecode,operands,semantics = $1,$2,$3.strip
       printf "  OP_%-11s= 0x%02x, //!< %-5s%s\n", opecode, opecode_num, operands, semantics
+#      printf "    case OP_%s:%sret = op_%-10s(vm, regs); break;\n",
+#             opecode, " " * (11 - opecode.size), opecode.downcase
       opecode_num += 1
     end
   end
